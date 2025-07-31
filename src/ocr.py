34a0c39f0a227680511253f3421619a1a606d8de
ayush-hverma <ayu5hh.verma03@gmail.com>
2025-08-01 01:51:59 +0530

    switched to TrOCR

--- src/ocr.py
+++ src/ocr.py
@@ -1,9 +1,12 @@
-import easyocr
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from PIL import Image
 
-reader = easyocr.Reader(['en'], gpu=False)
+processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-stage1")
+model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-stage1")
 
 def extract_text_from_image(image_path):
-    result = reader.readtext(image_path, detail=0)
-    text = "\n".join(result)
-    return text
+    image = Image.open(image_path).convert("RGB")
+    pixel_values = processor(images=image, return_tensors="pt").pixel_values
+    generated_ids = model.generate(pixel_values)
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    return generated_text
