ae6e5b9aac2c60ab5806d8c302183c831c6a901c
ayush-hverma <ayu5hh.verma03@gmail.com>
2025-08-01 10:53:36 +0530

    fix

--- src/ocr.py
+++ src/ocr.py
@@ -1,8 +1,13 @@
 import pytesseract
 from PIL import Image
+import platform
 
-# Set this only on Windows
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+# Dynamically set tesseract path
+if platform.system() == "Windows":
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+else:
+    # On Linux (Render uses Debian-based Linux)
+    pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
 
 def extract_text_from_image(image_path):
     image = Image.open(image_path)
